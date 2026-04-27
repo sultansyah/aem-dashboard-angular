6d2a8c3ee49860765ec456bf1bb119e0dfcefe68
sultansyah <muhammadsultansyah14@gmail.com>
2026-04-27 18:28:06 +0700

    feat: update env dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://test-demo.aemenersol.com/api',
-  enableLocalAuthFallback: false,
+  enableLocalAuthFallback: true,
   useHashRouting: false
 };
 
